11391b638c7098e0aa5342e2a366df93c8c03623
Andre Gurgel <andregurgeldev@gmail.com>
2024-06-18 22:55:56 -0300

    fix: Corrigindo mensagem de validação.

--- src/main/java/br/dev/andregurgel/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/dev/andregurgel/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -14,13 +14,13 @@ public class CandidateEntity {
     private UUID id;
     private String name;
 
-    @Pattern(regexp = "\\S+", message = "O campo 'username' não deve conter espaços em branco")
+    @Pattern(regexp = "\\S+", message = "O campo 'username' não deve conter espaços em branco.")
     private String username;
 
-    @Email(message = "O campo 'email' deve conter um e-mail válido")
+    @Email(message = "O campo 'email' deve conter um e-mail válido.")
     private String email;
 
-    @Length(min = 10, max = 100)
+    @Length(min = 10, max = 100, message = "O campo 'password' deve ter entre 10 e 100 caracteres.")
     private String password;
     private String description;
     private String curriculum;
