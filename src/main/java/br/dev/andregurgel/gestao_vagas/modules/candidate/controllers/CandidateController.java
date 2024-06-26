141e2df3caa27e78cd929f4adeab72f7f972b3b6
Andre Gurgel <andregurgeldev@gmail.com>
2024-06-25 21:50:04 -0300

    feat: Use Case.

--- src/main/java/br/dev/andregurgel/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/dev/andregurgel/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,14 +1,14 @@
 package br.dev.andregurgel.gestao_vagas.modules.candidate.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.dev.andregurgel.gestao_vagas.exceptions.CandidadeAlreadyExistsExeception;
 import br.dev.andregurgel.gestao_vagas.modules.candidate.CandidateEntity;
-import br.dev.andregurgel.gestao_vagas.modules.candidate.CandidateRepository;
+import br.dev.andregurgel.gestao_vagas.modules.candidate.service.CandidateService;
 import jakarta.validation.Valid;
 
 @RestController
@@ -16,14 +16,14 @@ import jakarta.validation.Valid;
 public class CandidateController {
 
     @Autowired
-    private CandidateRepository candidateRepository;
+    private CandidateService candidateService;
 
     @PostMapping
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
-        .ifPresent(candidate -> {
-            throw new CandidadeAlreadyExistsExeception();
-        });
-        return this.candidateRepository.save(candidateEntity);
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        try {
+            return ResponseEntity.ok().body(this.candidateService.execute(candidateEntity));
+        } catch(Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
